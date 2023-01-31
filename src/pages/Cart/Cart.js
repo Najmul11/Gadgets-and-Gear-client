166ce8880ec2e052c2403b467b9c38560086d42c
Najmul11 <najmulbinnurul@gmail.com>
2023-01-31 17:04:01 +0600

    delete consoles

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext} from 'react';
 import { toast } from 'react-hot-toast';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { CartContext } from '../../App';
